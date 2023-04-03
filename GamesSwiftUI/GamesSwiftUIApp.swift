126fc5c728cdbcd01458d06f7cdadfc6f81ed80a
Berkay Sancar <sancarberkay@gmail.com>
2023-04-03 13:53:21 +0300

    Initial Commit

---
+++ GamesSwiftUI/GamesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesSwiftUIApp.swift
+//  GamesSwiftUI
+//
+//  Created by Berkay Sancar on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GamesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
